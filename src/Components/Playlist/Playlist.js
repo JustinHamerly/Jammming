8c6327ebe98a6ec74048eaf7c93f8919326f0850
Justin Hamerly <suvimountain@icloud.com>
2022-05-06 11:44:33 -0700

    tracklist component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Playlist.css'
+import TrackList from '../TrackList/TrackList'
 
 const Playlist = () => {
   return (
     <div className="Playlist">
       <input defaultValue={'New Playlist'}/>
-      {/* <>Tracklist Component </> */}
+      <TrackList />
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
   )
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './SearchResults.css'
+import TrackList from '../TrackList/TrackList'
 
 const SearchResults = () => {
   return (
     <div className="SearchResults">
       <h2>Results</h2>
-      <>Tracklist Component</>
+      <TrackList />
     </div>
   )
 }
